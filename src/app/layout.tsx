1e040866eba62ee064c82e31b1a95d6ae4fe32c0
jagaa999 <jargal@kt.mn>
2025-04-09 17:59:34 +0800

    fixed Meta Data for Social Preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,30 @@ export const metadata = {
   icons: {
     icon: "/favicon.png",
   },
+  openGraph: {
+    title: "Special Mining",
+    description:
+      "Special Mining Services LLC (SMS) is a mining technology company that established in 2012 and is specialized in Drill & Blast and industrial chemicals. We are truly committed to safety, responsible operation and our clients’ interest.",
+    url: "https://www.special.mn", // ← өөрийн домэйнийг оруул
+    siteName: "Special Mining",
+    images: [
+      {
+        url: "/images/dddd.jpg", // ← public дотор байрлах social preview зураг
+        width: 1200,
+        height: 630,
+        alt: "Special Mining preview",
+      },
+    ],
+    locale: "mn_MN",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Special Mining",
+    description: "Монголын уул уурхайн салбарын шийдэл.",
+    images: ["/images/dddd.jpg"],
+    creator: "@Special", // Twitter хэрэглэгчийн нэр байгаа бол
+  },
 };
 
 export default function RootLayout({ children }: any) {
