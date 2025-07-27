c56a7851fe33adff3af52b75882ab07d414eb55f
jagaa999 <jargal@kt.mn>
2025-07-27 22:01:38 +0800

    ddd

--- atomv2/components/Organisms/OrganismNavbarMenu.tsx
+++ atomv2/components/Organisms/OrganismNavbarMenu.tsx
@@ -4,10 +4,8 @@ import RenderAtom from "atomv2/components/Atoms/RenderAtom";
 import BlockDiv from "atomv2/components/Blocks/BlockDiv";
 import PanelMain from "atomv2/components/Panel/PanelMain";
 import { ObjectLight } from "atomv2/types/objectTypes";
-import _, { map } from "lodash";
-import Link from "next/link";
+import { map } from "lodash";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 import { useWindowScroll } from "react-use";
 import BlockFlexRow from "../Blocks/BlockFlexRow";
 import OrganismUserAvatar from "./OrganismUserAvatar";
@@ -45,126 +43,69 @@ export default function OrganismNavbarMenu({
         scrolled ? "py-2" : "py-3"
       } ${variantClass} ${Outer?.className || ""}`}>
       <PanelMain className="py-0">
-        <BlockDiv className="w-full flex justify-between items-center">
-          <RenderAtom
-            value={item?.logo}
-            type="image"
-            className={`${
-              scrolled ? "w-[30px]" : "w-[50px]"
-            } h-auto object-contain rounded-none`}
-            alt="Main Logo"
-            url={{ href: "/" }}
-          />
-          <BlockFlexRow className="">
-            <RightMenu item={item} />
-            <OrganismUserAvatar />
-          </BlockFlexRow>
-        </BlockDiv>
+        <BlockFlexRow className="w-full justify-between">
+          <LeftPanel item={item} scrolled={scrolled} />
+          <RightPanel item={item} scrolled={scrolled} />
+        </BlockFlexRow>
       </PanelMain>
     </BlockDiv>
   );
 }
 
-const RightMenu = ({ item }: { item: any }) => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const pathname = usePathname();
-
-  useEffect(() => {
-    setMobileMenuOpen(false);
-  }, [pathname]);
-
+const LeftPanel = ({ item, scrolled }: { item: any; scrolled: boolean }) => {
   return (
-    <>
-      {/* Desktop Menu */}
-      <DesktopMenu item={item} />
-      {/* Mobile menu button */}
-      <button
-        className="md:hidden"
-        onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
-        {mobileMenuOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        )}
-      </button>
-
-      {/* Mobile Menu */}
-      <BlockDiv
-        className={`md:hidden mt-4 transition-all duration-300 overflow-hidden ${
-          mobileMenuOpen ? "max-h-[1000px]" : "max-h-0"
-        }`}>
-        <ul className="flex flex-col gap-2">
-          {_.map(item?.menu, (item: any, index: number) => (
-            <li key={index}>
-              <Link
-                href={item.href}
-                className={`block px-4 py-2 font-semibold text-[14px] ${
-                  pathname === item.href ? "text-brand" : "hover:text-brand"
-                }`}>
-                {item.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </BlockDiv>
-    </>
+    <RenderAtom
+      value={item?.logo}
+      type="image"
+      className={`${
+        scrolled ? "w-[30px]" : "w-[50px]"
+      } h-auto object-contain rounded-none`}
+      alt="Main Logo"
+      url={{ href: "/" }}
+    />
   );
 };
 
-const DesktopMenu = ({ item }: { item: any }) => {
-  const pathname = usePathname();
-
+const RightPanel = ({ item, scrolled }: { item: any; scrolled: boolean }) => {
   return (
-    <ul className="hidden md:flex flex-nowrap gap-x-6 gap-y-1 items-center h-full">
+    <BlockFlexRow>
+      <MainMenu item={item} scrolled={scrolled} />
+      <OrganismUserAvatar />
+    </BlockFlexRow>
+  );
+};
+
+const MainMenu = ({ item, scrolled }: { item: any; scrolled: boolean }) => {
+  const pathname = usePathname();
+  return (
+    <BlockFlexRow className="gap-x-6 gap-y-1 items-center h-full">
       {map(item?.menu, (item: any, index: number) => (
-        <li key={item?.id || index}>
-          <RenderAtom
-            value={item?.title}
-            type="text"
-            className={`flex items-center text-sm font-bold tracking-wide transition-all ${
-              pathname === item.href ? "text-brand" : "hover:text-brand"
-            }`}
-            url={{ href: item.href }}
-          />
-        </li>
+        <RenderAtom
+          key={item?.id || index}
+          value={item?.title}
+          type="text"
+          className={`flex items-center text-sm font-bold tracking-wide transition-all ${
+            pathname === item.href ? "text-brand" : "hover:text-brand"
+          }`}
+          url={{ href: item.href }}
+        />
       ))}
-      {item?.inquiryButton && (
-        <li>
-          <RenderAtom
-            value={item.inquiryButton.title}
-            type="text"
-            variant="action2"
-            className={
-              "px-3 py-[5px] rounded-full bg-brand text-white text-sm font-semibold"
-            }
-            url={{ href: item.inquiryButton.href }}
-          />
-        </li>
+
+      {item?.inquiryButton && !scrolled && (
+        <RenderAtom
+          value={item.inquiryButton.title}
+          type="text"
+          variant="action2"
+          className={
+            "px-3 py-[5px] rounded-full bg-brand text-white text-sm font-semibold"
+          }
+          url={{ href: item.inquiryButton.href }}
+          animation={{
+            type: "fadeIn",
+            duration: 1.5,
+          }}
+        />
       )}
-    </ul>
+    </BlockFlexRow>
   );
 };
--- atomv2/components/Organisms/OrganismUserAvatar.tsx
+++ atomv2/components/Organisms/OrganismUserAvatar.tsx
@@ -3,10 +3,11 @@
 import RenderAtom from "atomv2/components/Atoms/RenderAtom";
 import { useAuth } from "src/config/hooks/useAuth";
 import BlockGuestUser from "../Blocks/BlockGuestUser";
+import { useToggle } from "react-use";
+import BlockPopover from "../Blocks/BlockPopover";
+import { Menu } from "antd";
 
 export default function OrganismUserAvatar() {
-  const { motoUser } = useAuth();
-
   return (
     <BlockGuestUser>
       <RenderAtom
@@ -15,14 +16,66 @@ export default function OrganismUserAvatar() {
         className={`w-auto h-8 object-contain rounded-full`}
       />
 
+      <UserMenu />
+    </BlockGuestUser>
+  );
+}
+
+const UserMenu = () => {
+  const { motoUser } = useAuth();
+  const [isShowPopover, setIsShowPopover] = useToggle(false);
+  return (
+    <BlockPopover
+      isShowPopover={isShowPopover}
+      setIsShowPopover={setIsShowPopover}
+      // content={<MotoCustomerMenuPanelV3 />}
+      content={
+        <>
+          <UserMenuList />
+        </>
+      }
+      props={{
+        title: `Сайн уу? ${motoUser?.title}`,
+        trigger: "click",
+      }}>
       <RenderAtom
         value={motoUser?.mainimage}
         type="image"
         className={`w-auto h-8 object-contain rounded-full`}
         alt="User Avatar"
       />
-    </BlockGuestUser>
+    </BlockPopover>
   );
-}
+};
 
-// mdi:user
+const UserMenuList = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <RenderAtom
+          value="Profile"
+          type="text"
+          className="cursor-pointer hover:bg-gray-100 p-2 rounded"
+        />
+        <RenderAtom
+          value="Settings"
+          type="text"
+          className="cursor-pointer hover:bg-gray-100 p-2 rounded"
+        />
+        <RenderAtom
+          value="Logout"
+          type="text"
+          className="cursor-pointer hover:bg-gray-100 p-2 rounded"
+        />
+        {/* Ant Design Menu */}
+        <div className="mt-4">
+          <Menu>
+            <Menu.Item key="profile">Profile</Menu.Item>
+            <Menu.Item key="settings">Settings</Menu.Item>
+            <Menu.Item key="logout">Logout</Menu.Item>
+          </Menu>
+        </div>
+      </div>
+    </>
+  );
+};
